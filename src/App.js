b4d5663d2bba2222f3858c65493dc34fda3ae6f3
Avish Jain <avishjain0@gmail.com>
2023-02-27 23:18:39 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import Chart from "./Chart";
+import data from "./data.json";
+import { useEffect, useState } from "react";
+import moment from "moment/moment";
 
 function App() {
+  const [date, setDate] = useState(moment("2021-05-19").format("YYYY-MM-DD"));
+  const [schedule, setSchedule] = useState({});
+
+  useEffect(() => {
+    setSchedule({});
+    getScheduled();
+    console.log(schedule);
+  }, [date]);
+
+  function getDate(e) {
+    setDate(moment(e.target.value).format("YYYY-MM-DD"));
+    // console.log(moment("2021-05-18 12:39:29").format("YYYY-MM-DD"));
+  }
+
+  function getScheduled() {
+    data.forEach((order) => {
+      // console.log(order);
+      if (order.item_date === date) {
+        if (moment(order.schedule_time).format("YYYY-MM-DD") in schedule) {
+          console.log(moment(order.schedule_time).format("YYYY-MM-DD"));
+          // schedule[moment(order.schedule_time).format("YYYY-MM-DD")] += 1;
+          setSchedule({
+            ...schedule,
+            [moment(order.schedule_time).format("YYYY-MM-DD")]:
+              moment(order.schedule_time).format("YYYY-MM-DD") + 1,
+          });
+        } else {
+          setSchedule({
+            ...schedule,
+            [moment(order.schedule_time).format("YYYY-MM-DD")]: 1,
+          });
+        }
+      }
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="date"
+        value={date}
+        placeholder="Enter Input-Date"
+        onChange={getDate}
+      />
+      <div className="graph">
+        <Chart />
+      </div>
     </div>
   );
 }
---
+++ src/Chart.js
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const Chart = ({ month, scheduled }) => {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "right",
+      },
+      title: {
+        display: true,
+        text: "Chart.js Horizontal Bar Chart",
+      },
+    },
+  };
+
+  const data = {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 205, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(201, 203, 207, 0.2)",
+        ],
+        borderColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return <Bar data={data} options={options} />;
+};
+
+export default Chart;
